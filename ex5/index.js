d49efb152d6f5ee54e95543bf4284c675bcb8294
Tiago Silva <tiagosik1994@gmail.com>
2021-10-12 19:15:48 +0100

    feat: add jsonify when printing objects create in arraytolist

--- ex5/index.js
+++ ex5/index.js
@@ -53,11 +53,11 @@ function nth(list, index){
     return nth(list.rest, --index)
 }
 
-console.log(arrayToList([10, 20]));
+console.log(JSON.stringify(arrayToList([10, 20])));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(JSON.stringify(listToArray(arrayToList([10, 20, 30]))));
 // → [10, 20, 30]
-console.log(prepend(10, prepend(20, null)));
+console.log(JSON.stringify(prepend(10, prepend(20, null))));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
