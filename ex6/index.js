54f1cf3483188e664defff49ad924fdfcc22e8a7
Tiago Silva <tiagosik1994@gmail.com>
2021-10-18 21:30:07 +0100

    fix: add default value to reduce

--- ex6/index.js
+++ ex6/index.js
@@ -1,7 +1,7 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
 function flatten(array) {
-    return array.reduce((p, c) => p.concat(c))
+    return array.reduce((p, c) => p.concat(c), [])
 }
 // → [1, 2, 3, 4, 5, 6]
 arrays = flatten(arrays)
