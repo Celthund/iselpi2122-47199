5b5aa2df79a148ba038ec52f83e2a975ea2436df
Tiago Silva <tiagosik1994@gmail.com>
2021-10-23 16:19:38 +0100

    tests: add tests for testing writing and reading from files of ids and game infos

--- assignments/assignment_1/__test__/foreword.test.js
+++ assignments/assignment_1/__test__/foreword.test.js
@@ -1,9 +1,24 @@
 'use strict'
 
 const { expect } = require('@jest/globals')
-const { getGamesInfo, getGamesInfoPromise, Game, responseToGameArray } = require('../lib/foreword.js')
-
-
+const { getGamesInfo, getGamesInfoPromise, Game, responseToGameArray, gamesInfoFromIdsFileAsync, gamesInfoFromIdsFilePromise } = require('../lib/foreword.js')
+const fsPromises = require('fs').promises
+jest.mock("fs", () => ({
+  promises: {
+    writeFile: jest.fn((filename, data)=> new Promise((resolve, reject) => {
+      if(filename == "output.json")
+        resolve(data)
+      else
+        reject(Error("Filename doesn't exist"))
+    })),
+    readFile: jest.fn((filename, options) => new Promise((resolve, reject) => {
+      if(filename == "input.txt")
+        resolve("TAAifFP590\nyqR4PtpO8X\n5H5JS0KLzK")
+      else
+        reject(Error("Filename doesn't exist"))
+    })),
+  },
+}));
 
 test('Using Async/Await to get game info with 3 ids', async () => {
   // Root, Scythe, Wingspan
@@ -15,8 +30,7 @@ test('Using Async/Await to get game info with 3 ids', async () => {
     new Game("5H5JS0KLzK", "Wingspan", "https://www.boardgameatlas.com/game/5H5JS0KLzK/wingspan")
   ]
   expect(gamesInfo).toEqual(result)
-}
-)
+})
 
 test('Using Async/Await to get game info with 0 ids', async () => {
   // Root, Scythe, Wingspan
@@ -24,8 +38,7 @@ test('Using Async/Await to get game info with 0 ids', async () => {
   let gamesInfo = await getGamesInfo(gameids)
   const result = []
   expect(gamesInfo).toEqual(result)
-}
-)
+})
 
 test('Using Promises to get game info with 3 ids', () => {
   const gameids = ["TAAifFP590", "yqR4PtpO8X", "5H5JS0KLzK"]
@@ -36,22 +49,46 @@ test('Using Promises to get game info with 3 ids', () => {
   ]
   const promise = getGamesInfoPromise([gameids])
   return promise.then(value => expect(value).toEqual(result))
-}
-)
+})
 
 test('Using Promises to get game info with 0 ids', () => {
   const gameids = []
   const result = []
   const promise = getGamesInfoPromise(gameids)
   return promise.catch(value => expect(value).toEqual(result))
-}
-)
+})
 
 test('Testing responseToGameArray with empty object argument.', () => {
   expect(responseToGameArray({})).toEqual([])
 })
 
-
 test('Testing responseToGameArray with object with empty games property.', () => {
   expect(responseToGameArray({"games": []})).toEqual([])
+})
+
+test('Testing gamesInfoFromIdsFilePromise with invalid input file.', () => {
+  return gamesInfoFromIdsFilePromise("", "output.json").catch(res => expect(res).toEqual(Error ("Filename doesn't exist")))
+})
+
+test('Testing gamesInfoFromIdsFilePromise with valid input file and invalid output file.', () => {
+  return gamesInfoFromIdsFilePromise("input.txt", "").catch(res => expect(res).toEqual(Error ("Filename doesn't exist")))
+})
+ 
+test('Testing gamesInfoFromIdsFilePromise with valid input file and output file.', () => {
+  return gamesInfoFromIdsFilePromise("input.txt", "output.json").then(value => expect(value).toEqual(true))
+})
+
+test('Testing gamesInfoFromIdsFileAsync with invalid input file.', async () => {
+  const result = await gamesInfoFromIdsFileAsync("", "output.json")
+  expect(result).toEqual(false)
+})
+
+test('Testing gamesInfoFromIdsFileAsync with valid input file and invalid output file.', async () => {
+  const result = await gamesInfoFromIdsFileAsync("input.txt", "")
+  expect(result).toEqual(false)
+})
+ 
+test('Testing gamesInfoFromIdsFileAsync with valid input file and output file.', async () => {
+  const value = await gamesInfoFromIdsFileAsync("input.txt", "output.json")
+  expect(value).toEqual(true)
 })
