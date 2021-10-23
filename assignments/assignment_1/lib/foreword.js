d978edbb8532d0892649cc313613d40b63b3508f
Tiago Silva <tiagosik1994@gmail.com>
2021-10-23 16:19:10 +0100

    feat: add function to read ids from file and write in output file the game infos

--- assignments/assignment_1/lib/foreword.js
+++ assignments/assignment_1/lib/foreword.js
@@ -1,16 +1,13 @@
-const fetch = require('node-fetch');
+const fetch = require('node-fetch')
+const fsPromises = require('fs').promises
 
 module.exports = {
-    'getGamesInfo': getGamesInfo,
-    'getGamesInfoPromise': getGamesInfoPromise,
+    'getGamesInfo': fetchGameInfoAsync,
+    'getGamesInfoPromise': fetchGameInfoPromise,
     'Game': Game,
-    'responseToGameArray': responseToGameArray
-}
-
-function getUrl(gameids) {
-    const ids = gameids.reduce((prev, curr) => prev + "," + curr)
-    const url = "https://api.boardgameatlas.com/api/search?client_id=iLW2r2Ar8g&ids="
-    return url + ids
+    'responseToGameArray': objectArrayToGameArray,
+    'gamesInfoFromIdsFileAsync': gamesInfoFromIdsFileAsync,
+    'gamesInfoFromIdsFilePromise': gamesInfoFromIdsFilePromise
 }
 
 function Game(id, name, url) {
@@ -19,25 +16,53 @@ function Game(id, name, url) {
     this.url = url
 }
 
-function responseToGameArray(data) {
+function getUrl(gameids) {
+    const ids = gameids.reduce((prev, curr) => prev + "," + curr)
+    const url = "https://api.boardgameatlas.com/api/search?client_id=iLW2r2Ar8g&ids="
+    return url + ids
+}
+
+function objectArrayToGameArray(data) {
     if (!data["games"]) return []
     return data["games"].map((value) => new Game(value["id"], value["name"], value["url"]))
 }
 
-/**
- *
- * @param {Array} gameids
- */
-async function getGamesInfo(gameids) {
+async function fetchGameInfoAsync(gameids) {
     if (gameids.length == 0) return []
     const response = await fetch(getUrl(gameids))
     const data = await response.json()
-    return responseToGameArray(data)
+    return objectArrayToGameArray(data)
 }
 
-function getGamesInfoPromise(gameids) {
+function fetchGameInfoPromise(gameids) {
     if (gameids.length == 0) return new Promise((resolve) => resolve([]))
     return fetch(getUrl(gameids))
-                .then(response => response.json())
-                .then(json => responseToGameArray(json))
+        .then(response => response.json())
+        .then(json => objectArrayToGameArray(json))
+}
+
+async function gamesInfoFromIdsFileAsync(filename_origin, filename_destination) {
+    try {
+        const data = await fsPromises.readFile(filename_origin, {"encoding": "utf-8"})
+        const gameids = data.split(/\r?\n/)
+        const games = fetchGameInfoAsync(gameids)
+        await fsPromises.writeFile(filename_destination, JSON.stringify(games))
+        return true
+    } catch {
+        return false
+    }
+}
+
+function gamesInfoFromIdsFilePromise(filename_origin, filename_destination) {
+    return fsPromises.readFile(filename_origin, {"encoding": "utf-8"}).then(
+        (data) => {
+            const gameids = data.split(/\r?\n/)
+            return fetchGameInfoPromise(gameids)
+        })
+        .then(
+            (games) => {
+                return fsPromises.writeFile(filename_destination, JSON.stringify(games))
+        })
+        .then(_ => true)
+        .catch(_ => false)
 }
